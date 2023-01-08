eef169981d9abcb7de0e747a04a13022766d6549
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-01-07 19:19:15 -0500

    Move violation structs out of registry.rs

--- src/flake8_return/plugins.rs
+++ src/flake8_return/plugins.rs
@@ -8,7 +8,8 @@ use crate::autofix::Fix;
 use crate::checkers::ast::Checker;
 use crate::flake8_return::helpers::result_exists;
 use crate::flake8_return::visitor::{ReturnVisitor, Stack};
-use crate::registry::{Branch, DiagnosticCode};
+use crate::registry::DiagnosticCode;
+use crate::violations::Branch;
 use crate::{violations, Diagnostic};
 
 /// RET501
--- src/pyupgrade/plugins/native_literals.rs
+++ src/pyupgrade/plugins/native_literals.rs
@@ -5,8 +5,9 @@ use rustpython_parser::lexer::Tok;
 use crate::ast::types::Range;
 use crate::autofix::Fix;
 use crate::checkers::ast::Checker;
-use crate::registry::{Diagnostic, DiagnosticCode, LiteralType};
+use crate::registry::{Diagnostic, DiagnosticCode};
 use crate::violations;
+use crate::violations::LiteralType;
 
 /// UP018
 pub fn native_literals(
